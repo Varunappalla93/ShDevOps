9996bf32f5c5439b043b6c8e8fd7065edc4caaa2
Varunappalla93 <an.varun1993@gmail.com>
2023-12-12 15:26:30 +0530

    added fifth script

--- five.sh
+++ five.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-username=$1
+echo "Please enter your username"
+
+read username # the value entered above will be automatically attached to username variable
 password=$2
 
 echo "username is ${username} and password is ${password}"

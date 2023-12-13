3b1e4098b37f643cbcf448c13fe6038f4bece5a0
Varunappalla93 <an.varun1993@gmail.com>
2023-12-13 12:29:33 +0530

    modified six

--- six.sh
+++ six.sh
@@ -2,10 +2,9 @@
 number1=$1
 number2=$2
 
-sum=$((${number1}+${number2}))
-echo "total is ${sum}"
+sum=$(($number1+$number2))
+echo "total is $sum"
 
+echo "how many args are passed :: $#"
 
-echo "how many args are passed $#"
-
-echo "diplay all args passed $@"
+echo "diplay all args passed :: $@"

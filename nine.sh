3bb01abbbe3b5bc74976bbba3f820280ca0c4fa9
Varunappalla93 <an.varun1993@gmail.com>
2023-12-13 11:25:40 +0530

    nine

--- nine.sh
+++ nine.sh
@@ -5,9 +5,17 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "Please run the script with root access"
-    exit 1    # any no other than 0
+    exit 1    # give any no other than 0, to exit here
 else
     echo "you are root user."
 fi
 
 yum install mysql -y
+
+if [ $? -ne 0 ]:
+then 
+    echo "installing mySQL failed"
+    exit 1
+else
+    echo "installed mysql successfully"
+fi

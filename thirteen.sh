b921023081cb8027ddf184df19e8889a543f04e3
Varunappalla93 <an.varun1993@gmail.com>
2023-12-14 12:40:21 +0530

    13

---
+++ thirteen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#!/bin/bash
+
+ID=$(id -u)  # to check user id
+
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R Please run the script with root access $N"
+    exit 1    # give any no other than 0, to exit here
+else
+    echo "you are root user"
+fi
+
+echo "All arguments passed: $@"
+
+
+
+
+

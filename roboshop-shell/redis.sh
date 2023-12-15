ae1940cb1355147e0729f72eb14ee1abcfd32022
Varunappalla93 <an.varun1993@gmail.com>
2023-12-15 13:17:17 +0530

    redis

--- roboshop-shell/redis.sh
+++ roboshop-shell/redis.sh
@@ -47,7 +47,7 @@ dnf install redis -y
 
 VALIDATE $? "Install redis"
 
-sed -i 's/127\.0\.0\.1/0\.0\.0\.0/' /etc/redis.conf 
+sed -i 's/127\.0\.0\.1/0\.0\.0\.0/' /etc/redis/redis.conf 
 
 VALIDATE $? "allowing remote connections"
 

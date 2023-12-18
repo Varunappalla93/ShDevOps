954bf7289b8b2fa09de6e4efaaf106d6ad17d991
Varunappalla93 <an.varun1993@gmail.com>
2023-12-18 14:18:37 +0530

    whileloop

---
+++ sixteen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+R="\e[31m",
+G="\e[32m",
+Y="\e[33m",
+N="\e[0m",
+
+
+
+
+if [ ! -d $src_directory ] # ! is opposite
+then
+    echo "$R src directory : $src_directory does not exist. $N"
+fi
+
+
+file="/etc/passwd"
+while IFS=":" read -r $username $password $userid $groupid $userfullname $homedirectory $shellpath
+do
+    echo "username : $username"
+    echo "userid:  $userid"
+    echo "user full name : $userfullname"
+done < $file
+

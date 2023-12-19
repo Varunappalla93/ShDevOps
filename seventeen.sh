37d63005c9449ac32f12f6718866a10e90b3b4f2
Varunappalla93 <an.varun1993@gmail.com>
2023-12-19 09:22:20 +0530

    diskUsage

--- seventeen.sh
+++ seventeen.sh
@@ -11,7 +11,7 @@ do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
 
-    if [ $$usage -ge $partition ]
+    if [ $$usage -ge $DISK_THRESHHOLD ]
     then
         message+="High Disk Usage on $partition:$usage \n"
     fi

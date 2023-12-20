834ffe3eb7870a1a0de9f5b8a63a4806f617e5d4
Varunappalla93 <an.varun1993@gmail.com>
2023-12-20 15:05:26 +0530

    18

--- eighteen.sh
+++ eighteen.sh
@@ -8,14 +8,14 @@ USAGE()
     echo "USAGE :: "$(basename $0) -n <name> -w <wishes>"
     echo "Options ::"
     echo " -n, Specify the name (mandatory)"
-    echo " -w, Specify the wishes (mandatory)", eg: Good Morning
+    echo " -w, Specify the wishes, eg: Good Morning"
     echo " -h, Display Help and Exit"
 }
 
 while getopts ":n:w:h" opt;do
     case $opt in 
-        n) NAME="$$OPTARG";;
-        w) WISHES="$OPTARG";
+        n) NAME="$OPTARG";;
+        w) WISHES="$OPTARG";;
         h|*) USAGE; exit;;
     esac
 done

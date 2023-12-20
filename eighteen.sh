431000c32c8dd426cca45787258f53a6aed117b4
Varunappalla93 <an.varun1993@gmail.com>
2023-12-20 15:02:15 +0530

    18

--- eighteen.sh
+++ eighteen.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-PERSON=$1
+NAME=""
+WISHES=""
 
-echo "HELLO $PERSON, We are learning shell"
+USAGE()
+{
+    echo "USAGE :: "$(basename $0) -n <name> -w <wishes>"
+    echo "Options ::"
+    echo " -n, Specify the name (mandatory)"
+    echo " -w, Specify the wishes (mandatory)", eg: Good Morning
+    echo " -h, Display Help and Exit"
+}
+
+while getopts ":n:w:h" opt;do
+    case $opt in 
+        n) NAME="$$OPTARG";;
+        w) WISHES="$OPTARG";
+        h|*) USAGE; exit;;
+    esac
+done

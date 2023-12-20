0a6d9b7b3b13a1f8a448d2bf1b0fe61767d422ed
Varunappalla93 <an.varun1993@gmail.com>
2023-12-20 16:32:08 +0530

    instancecreation

--- roboshop-shell/roboshop.sh
+++ roboshop-shell/roboshop.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-AMI_ID=ami-03265a0778a880afb 
+AMI=ami-03265a0778a880afb 
 SG_ID=sg-00217dc8cbd2f0637
-INSTANCES=("mongodb","redis","mysql","rabbitmq","catalogue","user","cart","shipping","payment","dispatch","web")
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+# ZONE_ID=Z104317737D96UJVA7NEF
+# DOMAIN_NAME="daws76s.online"
 
 for i in "${INSTANCES[@]}"
 do
-    echo "instance is : $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
@@ -16,8 +17,5 @@ do
 
     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-00217dc8cbd2f0637
 done
-
-
-
 
 

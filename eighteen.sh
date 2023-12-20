9aefb1dc11b9b2923a5b988662a09ca2f438773b
Varunappalla93 <an.varun1993@gmail.com>
2023-12-20 15:06:27 +0530

    18

--- eighteen.sh
+++ eighteen.sh
@@ -5,7 +5,7 @@ WISHES=""
 
 USAGE()
 {
-    echo "USAGE :: "$(basename $0) -n <name> -w <wishes>"
+    echo "USAGE :: $(basename $0) -n <name> -w <wishes>"
     echo "Options ::"
     echo " -n, Specify the name (mandatory)"
     echo " -w, Specify the wishes, eg: Good Morning"
